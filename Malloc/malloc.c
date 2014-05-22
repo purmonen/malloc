b6a00af54bd15d8211109fd103c127ee06e955b6
Sami Purmonen <purmonen@kth.se>
2014-05-23 00:33:29 +0200

    Fixed mess

--- Malloc/malloc.c
+++ Malloc/malloc.c
@@ -1,142 +1,183 @@
-/*
- * Description:
- *
- * Save the address of the twilight zone.
- * First phase - allocate a MAXPOSTS number of memory blocks each
- *               of random size but of MAXSIZE/2.
- *              Check that zero size the returned pointer was NULL.
- *              Check that for non-zero size the pointer is not NULL.
- *              Fill the beginning of each block with a float.
- *
- * Computer memory usage
- *
- * Second phase - realloc randomly blocks requesting for sizes that either
- *		  can shrink or dilate the original block.
- *		  null blocks are allcated if necessary.
- *
- * Compute current break and used memory.
- * $Log: tstalgorithms.c,v $
- * Revision 1.3  1993/12/01  14:34:41  luis
- * Checks that NULL is returned for 0 bytes allocation, and that
- * non-zero is returned for non-zero bytes (trivial, but some malloc's
- * do it ...).
- *
- * Revision 1.2  1993/11/10  14:36:06  luis
- * Bug fixed. It didn't check that malloc returned NULL when size is zero.
- * An attempt to write on a NULL address generated SIGSEGV.
- *
- * Revision 1.1  1993/11/10  10:19:22  luis
- * Initial revision
- *
- */
-#include <stdlib.h>
-#include <stdio.h>
-#include <time.h>
 #include "malloc.h"
-#include "tst.h"
+#include <sys/mman.h>
+#include <stdio.h>
+#include <errno.h>
+#include <unistd.h>
+#include <math.h>
 
-#define MAXPOSTS 2000
-#define MAXSIZE  2048
-#define MAXITERS 10000
-#define ALLOCATED(i) (memPosts[i].size > 0)
+#define ALIGNMENT 8
 
-typedef struct
-{
-    double *ptr;
-    int size;
-} allocpost;
 
-allocpost memPosts[MAXPOSTS];
+int takeCount = 0;
 
-void calcMemUsage(int *max)
-{
-    int sum=0,i;
-    for(i=0;i<MAXPOSTS;i++) sum += memPosts[i].size*sizeof(double);
-    if(sum > *max) *max = sum;
+struct Header {
+	struct Header *next;
+	size_t size;
+} typedef Header;
+
+Header *freeList = NULL;
+
+
+void printList() {
+	int i = 0;
+	printf("THIS IS HOW THE LIST LOOKS RIGHT NOW\n");
+	for (;freeList != NULL; freeList = freeList->next) {
+		printf("%d, size: %ld, address: %p\n", i, freeList->size, freeList);
+		i++;
+	}
 }
 
-int main(int argc, char *argv[])
-{
-    int i, maxMem=0;
-    void *start, *end;
-    char *progname;
+Header * getNextHeader(long start, long end) {
+
     
-    if (argc > 0)
-        progname = argv[0];
-    else
-        progname = "";
-    MESSAGE("-- This test checks malloc(), free() and realloc()\n");
-    srand((unsigned int)time(NULL));
-    
-#ifdef MMAP
-    start = endHeap();
-#else
-    start = (void *)sbrk(0);
-#endif
-    
-    for(i=0;i<MAXPOSTS;i++)
-    {
-        memPosts[i].size = rand()%(MAXSIZE/2);
-        memPosts[i].ptr = (double*) malloc(memPosts[i].size*sizeof(double));
-        if ( memPosts[i].size == 0 &&  memPosts[i].ptr!= NULL )
-        /*	MESSAGE("* ERROR: malloc doesn't return NULL pointer on zero size\n");*/
-            ;
-        else if( memPosts[i].size && memPosts[i].ptr == NULL ) {
-            MESSAGE("* ERROR: malloc returned NULL on non-zero size request\n");
-        }
-        else if( memPosts[i].ptr != NULL )
-            memPosts[i].ptr[0]  = (double)3.14;
+	long nextPosition = start - (start % ALIGNMENT);
+    if (nextPosition < start) {
+        nextPosition += ALIGNMENT;
     }
+	long emptySpace = end - nextPosition - sizeof(Header);
+    if (emptySpace <= 0) return NULL;
     
-    calcMemUsage(&maxMem);
-    
-    for(i=0;i<MAXITERS;i++)
-    {
-        int index;
-        index = rand()%MAXPOSTS;
-        
-        if(ALLOCATED(index))
-        {
-            if(rand()%5 < 3)
-            {
-                if(memPosts[index].ptr[0] != (double)3.14)
-                    MESSAGE("* ERROR: Corrupt memory handling\n");
-                memPosts[index].size = rand()%MAXSIZE;
-                memPosts[index].ptr =
-                (double*) realloc(memPosts[index].ptr,
-                                  memPosts[index].size*sizeof(double));
-                if(memPosts[index].size && memPosts[index].ptr[0] != (double)3.14)
-                    MESSAGE("* ERROR: Corrupt memory handling\n");
-                if(memPosts[index].size) memPosts[index].ptr[0] = (double)3.14;
-            }
-            else
-            {
-                if(memPosts[index].ptr[0] != (double)3.14)
-                    MESSAGE("* ERROR: Corrupt memory handling\n");
-                free(memPosts[index].ptr);
-                memPosts[index].size = 0;
-            }
-        }
-        else
-        {
-            memPosts[index].size = rand()%MAXSIZE;
-            memPosts[index].ptr = (double*) malloc(memPosts[index].size*sizeof(double));
-            if(memPosts[index].size) memPosts[index].ptr[0] = (double)3.14;
-        }
-        calcMemUsage(&maxMem);
+    if (emptySpace > 80 * 1000) {
+        printf("HMM!");
     }
-#ifdef MMAP
-    end = endHeap();
-#else
-    end = (void *) sbrk(0);
-#endif
-    
-    fprintf(stderr,
-            "%s: Max memory allocated %d\n%s: Memory consumed %ld\n",
-            progname,
-            maxMem,
-            progname,
-            (unsigned long)(end-start));
-    return 0;
+    Header *header = (Header *)nextPosition;
+    header->size = emptySpace;
+    header->next = NULL;
+    return header;
 }
 
+void insertIntoFreeList(Header *header) {
+    if (header->size > 80 * 1000) {
+        printf("THE HELL!");
+    }
+    Header *current = freeList;
+    Header *prev = NULL;
+    while (current && current < header) {
+        prev = current;
+        current = current->next;
+    }
+    if (prev) {
+        prev->next = header;
+    } else {
+        freeList = header;
+    }
+    header->next = current;
+}
+
+void * takeEmptySpace(size_t size) {
+    takeCount++;
+	Header *prev = NULL;
+	Header *current = NULL;
+	for (current = freeList; current != NULL; current = current->next) {
+		if (current->size >= size) {
+            long start = (long)current + size + sizeof(Header);
+            long end = (long)current + current->size + sizeof(Header);
+            Header * nextHeader = getNextHeader(start, end);
+            if (nextHeader && nextHeader->size > 80 * 1000) {
+                printf("THE FUCK!\n");
+            }
+            if (nextHeader) {
+                if (!prev) {
+                    freeList = nextHeader;
+                } else {
+                    prev->next = nextHeader;
+                    nextHeader->next = current->next;
+                }
+            } else {
+                if (!prev) {
+                    freeList = current->next;
+                } else {
+                    prev->next = current->next;
+                }
+            }
+			return current+1;
+		}
+		prev = current;
+	}
+	return NULL;
+}
+
+
+void allocateMoreSpace(size_t size) {
+	long pageSize = sysconf(_SC_PAGESIZE);
+	long usedSize = (int)size + sizeof(Header);
+	long pages = usedSize / pageSize + 1;
+	long totalSize = pages * pageSize;
+	Header * header = mmap(NULL, totalSize, PROT_READ|PROT_WRITE, MAP_ANON|MAP_SHARED, -1, 0);
+    
+    if (!header) {
+        printf("NO HEADER\n");
+    }
+    
+    if (size > 8 * 10000) {
+        printf("LARGE MEMORY\n");
+    }
+    
+	header->size = size;
+    header->next = NULL;
+    
+	insertIntoFreeList(header);
+    
+    long start = (long)header + usedSize;
+    long end = (long)header + totalSize;
+    Header *nextHeader = getNextHeader(start, end);
+    
+    
+    if (nextHeader->size > 80 * 1000) {
+        printf("WTF\n");
+    }
+    
+    if (nextHeader) {
+        insertIntoFreeList(nextHeader);
+    }
+}
+
+void * malloc(size_t size) {
+    if (size <= 0) return NULL;
+	void * emptySpace = takeEmptySpace(size);
+	if (!emptySpace) {
+		allocateMoreSpace(size);
+        emptySpace = takeEmptySpace(size);
+		return emptySpace;
+	} else {
+		return emptySpace;
+	}
+}
+
+void * realloc(void *p, size_t size) {
+    if (!p) return malloc(size);
+    Header *header = (Header *)p - 1;
+    
+    void *data = malloc(size);
+    long minSize = size < header->size ? size : header->size;
+    long i;
+    for (i = 0; i < minSize; i++) {
+        ((char *)data)[i] = ((char *)p)[i];
+    }
+    //free(p);
+    return data;
+}
+
+void mergeList() {
+    if (!freeList) return;
+    Header *current = freeList;
+    while (current && current->next) {
+        if ((long)current + current->size + sizeof(Header) == (long)current->next) {
+            current->size += sizeof(Header) + current->next->size;
+            current->next = current->next->next;
+        }
+        current = current->next;
+    }
+}
+
+void free(void *p) {
+    if (!p) return;
+    Header *header = (Header *)p - 1;
+    if (header == freeList) return;
+    header->next = freeList;
+    if (header->size > 80 * 1000) {
+        printf("FUCKED ME UP");
+    }
+    freeList = header;
+    mergeList();
+}
